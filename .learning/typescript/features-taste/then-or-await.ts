e49332f6c4bd9873f2acd57b7f061ade9de3d228
yhm ypa <103625580+yhm-amber@users.noreply.github.com>
2023-03-04 00:27:50 +0800

    Update then-or-await.ts

--- .learning/typescript/features-taste/then-or-await.ts
+++ .learning/typescript/features-taste/then-or-await.ts
@@ -94,7 +94,7 @@ const pick_webhtml =
             ( htmldom => 
                 Array.from( htmldom.getElementsByClassName("reference internal") )
                     .map(elem => `${webmessage.mainpath}/${elem.getAttribute("href") ?? ""}`)
-                    .map(pathper => {return {per_path: pathper, main_hostpath: `${webmessage.sitehost}/${webmessage.mainpath}`, picker: webmessage.picker}})
+                    .map(pathper => {return {per_path: pathper, main_hostpath: `${webmessage.sitehost}`, picker: webmessage.picker}})
                     .map(msgper => pick_perhtml(msgper))
             )
             .then(htmparts_promising => Promise.all(htmparts_promising) )
@@ -104,4 +104,51 @@ const pick_webhtml =
 
 // 🙃
 
+
+// by normal, things in Promise cannot be used out of it.
+// but, you can use this :
+
+interface AsyncRes<T>
+{
+    is_complete: boolean ;
+    res?: T ;
+    err?: any ;
+} ;
+
+const run_async = 
+    <T,> (promising: Promise<T> | PromiseLike<T>)
+    : AsyncRes<T> =>
+    {
+        const result: AsyncRes<T> = {is_complete: false} ;
+        promising
+            .then
+            ( res => 
+            {
+                result.res = res ;
+                result.is_complete = true ; }
+            , (err: any) => 
+            {
+                result.err = err ;
+                result.is_complete = true ; }
+            )
+        
+        return result ;
+    } ;
+
+const async_res = run_async(picking) ;
+
+
+const iid = setInterval
+((): void =>
+{
+    if (async_res.is_complete)
+    {
+        clearInterval(iid) ;
+        if (async_res.err)
+        { console.log (`[err]: ${async_res.err}`) }
+        { console.log (`[res]: ${async_res.res}`) } ;
+    }
+}, 100)
+
+// known this by gpt ai: https://chatgpt.icloudnative.io/#/chat/1677822819602
 
