be3f7a0bf8b7d6ac2430e40b32c7c0ad62b86af5
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2023-11-08 09:53:25 +0800

    Create funs.r

---
+++ .learning/r/features-taste/funs.r
@@ -0,0 +1,11 @@
+`names<-` (1:2, c("a", "b")) ;
+## a b 
+## 1 2 
+
+(function (x, y) { attr (x, "names") <- y  ; x }) (1:2, c("a", "b")) ;
+## a b 
+## 1 2 
+
+(function (x, y) { names (x) <- y  ; x }) (1:2, c("a", "b")) ;
+## a b 
+## 1 2 
