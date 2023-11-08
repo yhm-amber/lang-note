4baacd1163209a1a80e110dd9dfb0b137376db77
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2023-11-09 06:49:09 +0800

    Update functions.r

--- .learning/r/features-taste/functions.r
+++ .learning/r/features-taste/functions.r
@@ -1,9 +1,9 @@
 `names<-` (1:2, c("a", "b")) ;
-## a b 
-## 1 2 
-
 (function (x, y) { attr (x, "names") <- y  ; x }) (1:2, c("a", "b")) ;
-
 (function (x, y) { names (x) <- y  ; x }) (1:2, c("a", "b")) ;
 ## a b 
 ## 1 2 
+
+`[<-` (1:3, 2, 99) ;
+(function (x) {function (y) {function (z) { x[y] <- z ; x }}}) (1:3) (2) (99) ;
+# [1]  1 99  3
