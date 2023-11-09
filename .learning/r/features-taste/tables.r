1556c57ca6fddc49fbdfb05a2b397877aee78eff
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2023-11-10 07:41:43 +0800

    Update tables.r

--- .learning/r/features-taste/tables.r
+++ .learning/r/features-taste/tables.r
@@ -9,6 +9,7 @@
 	funny.langs |> print();
 	`$<-` (funny.langs, "x", 2:5) |> print();
 	`$<-` (funny.langs, "xx", 2*2:5) |> print();
+	cbind (funny.langs, xxx = 3*2:5) |> print();
 }) () ;
 ##   rk lang.emoji lang.name
 ## 1  1         🐌        sh
@@ -25,3 +26,8 @@
 ## 2  2         🦀      rust  6
 ## 3  3         🐫     ocaml  8
 ## 4  4         🛷      wasm 10
+##   rk lang.emoji lang.name xxx
+## 1  1         🐌        sh   6
+## 2  2         🦀      rust   9
+## 3  3         🐫     ocaml  12
+## 4  4         🛷      wasm  15
