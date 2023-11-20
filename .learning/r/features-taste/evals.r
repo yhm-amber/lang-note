53c3432c935c53b5ab4d077a6decaf5cfcc5bc44
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2023-11-20 10:29:12 +0800

    Update evals.r

--- .learning/r/features-taste/evals.r
+++ .learning/r/features-taste/evals.r
@@ -15,3 +15,8 @@ expression (1 + 1) |> eval () ; # [1] 2
 "1 + 1" |> parse (text = _) |> eval () ;
 "1   +   1" |> parse (text = _) |> eval () ;
 # [1] 2
+
+(expression (\ (x) x) |> eval ()) (0)
+(expression (\ (x) x) |> eval (expr = _)) (0)
+(parse (text = "\\ (x) x") |> eval ()) (0)
+# [1] 0
