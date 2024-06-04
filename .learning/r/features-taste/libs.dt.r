e06dff081e542a418cbe1976279999d8a770c178
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-06-04 16:51:31 +0800

    Update libs.dt.r

--- .learning/r/features-taste/libs.dt.r
+++ .learning/r/features-taste/libs.dt.r
@@ -3,14 +3,37 @@
 #' 
 
 data.table::as.data.table(dplyr::starwars)[
-  i = T, 
-  j = .(h = mean(height), m = sum(mass)), 
-  by = .(species, homeworld)] -> a
+	i = T, 
+	j = .(h = mean(height), m = sum(mass)), 
+	by = .(species, homeworld)] -> a
 
-dtplyr::lazy_dt(dplyr::starwars) |> 
-  dplyr::group_by(species, homeworld) |> 
-  dplyr::summarise(h = mean(height), m = sum(mass)) |> 
-  data.table::as.data.table() -> b
+dtplyr::lazy_dt(dplyr::starwars, immutable = F) |> 
+	dplyr::group_by(species, homeworld) |> 
+	dplyr::summarise(h = mean(height), m = sum(mass)) |> 
+	data.table::as.data.table() -> b
 
-a|>diffdf::diffdf(b, keys = base::c("species", "homeworld"))
-# No issues were found!
+a |> diffdf::diffdf(b, keys = base::c("species", "homeworld")) # No issues were found!
+
+
+#' left join
+#' 
+
+tibble::tribble(
+	~ species_x,  	~ homeworld, 	~ choosing, 
+	  "Human",    	  "Naboo",   	  F, 
+	  "Gungan",   	  "Naboo",   	  T, 
+	  "Human",    	  "Stewjon", 	  T, 
+	  "Iktotchi", 	  "Naboo",   	  T) -> x
+
+data.table::copy(b) [i = x, on = base::c("species"="species_x", "homeworld"), j = choosing := choosing] -> a.j0
+data.table::as.data.table(x) [i = b, on = base::c("species_x"="species", "homeworld")] |> data.table::setnames(old = "species_x", new = "species") -> a.j1
+data.table::merge.data.table(x = b, y = x, by.x = base::c("species", "homeworld"), by.y = base::c("species_x", "homeworld"), all.x = T) -> a.j2
+
+a.j0 |> diffdf::diffdf(a.j2, keys = base::c("species", "homeworld")) # No issues were found!
+a.j1 |> diffdf::diffdf(a.j2, keys = base::c("species", "homeworld")) # No issues were found!
+
+dtplyr::lazy_dt(b, immutable = T) |> dplyr::left_join(x, by = base::c("species"="species_x", "homeworld")) |> data.table::as.data.table() -> b.j
+
+b.j |> diffdf::diffdf(a.j2, keys = base::c("species", "homeworld")) # No issues were found!
+
+
