f9b1e5c65f8c74c691589b7f6c8a2b6f8a9c5e3c
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-07-22 12:17:21 +0800

    Create packagebuilds.r

---
+++ .learning/r/features-taste/packagebuilds.r
@@ -0,0 +1,8 @@
+#' Gen/Up desc and docs
+devtools::document("package_dir")
+
+#' Make checks and fix problems by its hint
+devtools::check("package_dir")
+
+#' Build tarball into a dest path such as the parent dir
+devtools::build("package_dir", path = "..")
