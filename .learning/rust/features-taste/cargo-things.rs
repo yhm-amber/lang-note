7fefe8bdaa5adc18b430436fcdfbf2242a196038
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-07-23 22:30:18 +0800

    Update cargo-things.rs

--- .learning/rust/features-taste/cargo-things.rs
+++ .learning/rust/features-taste/cargo-things.rs
@@ -1,3 +1,9 @@
+
+// `cargo fetch -- ...`
 // see: 
 // - https://users.rust-lang.org/t/easiest-way-to-manually-download-a-crate-from-crates-io/67338/11
 // - https://stackoverflow.com/questions/76187501/is-there-a-cargo-command-exclusively-to-download-crates-to-the-registry-director
+
+// `cargo install --locked -- ...`
+// see: 
+// - https://www.reddit.com/r/rust/comments/qi1ntf/what_does_locked_do_in_cargo_install_pkg_locked/
