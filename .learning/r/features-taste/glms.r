a6314a4d42375bb5f7b1a87f8070098174e0f08d
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-07-23 17:24:48 +0800

    Create glms.r

---
+++ .learning/r/features-taste/glms.r
@@ -0,0 +1,3 @@
+#' see: 
+#' - https://stats.stackexchange.com/questions/130151/step-stats-is-too-slow-are-there-multicore-solutions
+#' - https://stats.stackexchange.com/questions/20836/algorithms-for-automatic-model-selection/20856#20856
