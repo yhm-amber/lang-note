2f1909d7f3b4626ef0d29e4a156bdf67d938af04
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-07-23 17:23:25 +0800

    Create cargo-things.rs

---
+++ .learning/rust/features-taste/cargo-things.rs
@@ -0,0 +1,3 @@
+// see: 
+// - https://users.rust-lang.org/t/easiest-way-to-manually-download-a-crate-from-crates-io/67338/11
+// - https://stackoverflow.com/questions/76187501/is-there-a-cargo-command-exclusively-to-download-crates-to-the-registry-director
