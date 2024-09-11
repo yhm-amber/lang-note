0c1b6b41cb674f7a69bd68d38354a584a4084f9c
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2024-09-11 17:59:05 +0800

    Update email.demo.r

--- .learning/r/features-taste/email.demo.r
+++ .learning/r/features-taste/email.demo.r
@@ -1,14 +1,50 @@
+
 #' emayili demo: 
 #' 
+{
+	email <- emayili::envelope(text = "OMG !!!!") |> 
+		emayili::from("sender@mail.server.from") |> 
+		emayili::to("receiver@mail.server.to") |> 
+		emayili::subject("This is a plain text message!")
 
-email <- emayili::envelope(text = "OMG !!!!") |> 
-	emayili::from("sender@mail.server.from") |> 
-	emayili::to("receiver@mail.server.to") |> 
-	emayili::subject("This is a plain text message!")
+	email |> 
+		emayili::server(
+			host = "server.mail.co",
+			port = 465,
+			username = "account@some.mail.co",
+			password = "(your passwd here)") (verbose = F) -> send_report
+}
 
-email |> 
-	emayili::server(
-		host = "server.mail.co",
-		port = 465,
-		username = "account@some.mail.co",
-		password = "(your passwd here)") (verbose = F) -> send_report
+#' blastula demo: 
+#' 
+{
+	img_path = base::system.file("img", "pexels-photo-267151.jpeg", package = "blastula")
+	body_template = "
+Hello,
+
+This is a *great* picture I found when looking
+for sun + cloud photos:
+
+{blastula::add_image(file = img_path)}"
+	
+	#' Image will trans to base64 by `blastula::add_image`.
+	#' 
+	#' You can see preview by just enter `email` at R Studio console after create it.
+	#' 
+	email <- blastula::compose_email(
+		body = blastula::md(glue::glue(body_template)), 
+		footer = blastula::md(glue::glue("Email sent on {blastula::add_readable_time()}.")))
+	
+	email |> 
+		blastula::smtp_send(
+			credentials = blastula::creds_envvar(
+				user = base::Sys.getenv('SMTP_USER'), 
+				pass_envvar = 'SMTP_PASSWORD', 
+				host = "server.mail.co", 
+				port = 465, 
+				use_ssl  = T), 
+			to = "receiver@mail.server.to", 
+			from = "sender@mail.server.from", 
+			subject = "Testing the `blastula::smtp_send()` function", 
+			verbose = F) -> report
+}
