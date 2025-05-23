2f52354e21035c53fec99e44805c9253bb216098
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-05-23 15:05:37 +0800

    Update symbol_play.R

--- .learning/r/ideas-act/symbol_play.R
+++ .learning/r/ideas-act/symbol_play.R
@@ -1,1 +1,2 @@
 syms_to_chr = function (...) rlang::ensyms(...) |> purrr::map_chr(base::as.character)
+terms_to_chr = function(...) base::as.list(base::substitute(base::list(...)))[-1L] |> purrr::map_chr(~ base::deparse(.x) |> base::paste(collapse = ''))
