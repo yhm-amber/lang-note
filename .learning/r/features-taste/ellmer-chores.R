07e4e49e1ddb9a8ba86051cca224ef7e90c9d0cf
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-11-14 16:20:44 +0800

    Create ellmer-chores.R

---
+++ .learning/r/features-taste/ellmer-chores.R
@@ -0,0 +1,48 @@
+
+#: Setting API for chores plugin
+options(.chores_chat = ellmer::chat_vllm(
+	base_url = Sys.getenv("ABCLLM_BASE_URL"),
+	api_key = Sys.getenv("ABCLLM_API_KEY"),
+	model = Sys.getenv("ABCLLM_MODEL")))
+
+#: Then you can select your function definition codes
+#:  and Press CTRL-SHIFT-P then search and click "Chores"
+#:  or Press a shortcut you've setted for "Chores", then
+#:  click "roxygen" from helpers list in the pop-up sub-window to
+#:  let the LLM generate doc-comments for this func.
+
+
+#: You can also define your own helper.
+
+#: Create an helper file
+#: Here to create an expounder which output it's answer
+#:  below the selected text (i.e., suffix).
+chores::prompt_new(
+	chore = "expounder", 
+	interface = "suffix")
+
+#: Run this command, RStudio will open a new .md file with a prompt template as its content.
+#: You can edit the prompt template as you like, then save and run that to load changes.
+chores::directory_load()
+
+#: Then you can try this magik !!
+
+#: You can open this file again by run
+chores::prompt_edit("expounder")
+
+
+#: Additionally, you can also add `tools` to your assistant
+
+#: This can give a `pwd` tool to your assistant
+#:  and any helpers are able to use it, as long as the selected prompt text
+#:  makes the LLM (model behind the assistant) believe it should be use at this time.
+'pwd' |> 
+	ellmer::tool(
+		fun = \ () { fs::path_wd() }, 
+		name = _, 
+		description = "Show work dir", 
+		arguments = list(), 
+		annotations = list()) |> 
+	base::getOption('.chores_chat')$register_tool()
+
+
