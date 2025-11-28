3cdf3be1cb3014df35e87b955bf39c122d8ec586
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-11-28 23:12:42 +0800

    Update chatlas-querychat.py

--- .learning/python/features-taste/chatlas-querychat.py
+++ .learning/python/features-taste/chatlas-querychat.py
@@ -9,6 +9,7 @@ chat = chatlas.ChatGroq(
 #: Prepare sqlalchemy engine
 from sqlalchemy import create_engine
 engine = create_engine('xyz://...')
+#: Upload `dfply.diamonds` onto that db as "diamonds".
 
 #: Create qc
 from querychat import QueryChat
