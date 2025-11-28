6f8a388963aebc8c34351f5ca48de95a3c19e93a
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-11-28 09:45:01 +0800

    Update switch.r

--- .learning/r/features-taste/switch.r
+++ .learning/r/features-taste/switch.r
@@ -1,8 +1,12 @@
 
-# todo: test
+#: Its param won't be eval before it be choosed.
 type |> base::switch(wee = fn_a(), woo = fn_b(type))
 
-# should same as
+#: means this can same as
 if (type |> base::identical('wee')) fn_a() else 
 if (type |> base::identical('woo')) fn_b(type) else 
 NULL ;
+
+#: Also type can be index number
+#:  rather than name-specify
+#:  like the aboves.
