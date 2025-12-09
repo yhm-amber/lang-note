3cb9a7eaa37dbc8e1ac8ff5370eb31cd38284193
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-12-09 17:08:12 +0800

    Update coro-nextingiter.R

--- .learning/r/features-taste/coro-nextingiter.R
+++ .learning/r/features-taste/coro-nextingiter.R
@@ -101,3 +101,66 @@ base::list2env(base::list(i = 9)) |> base::with({
 #| [1] "N"
 
 
+#: 准备数据集
+data.table::setDT(src <- dplyr::starwars |> dplyr::select(base::seq(11)))
+
+#: 把数据集当作向量循环播放（有断点） - 行
+base::list2env(base::list(i = 9)) |> base::with({
+	coro::loop(for (a in src |> generate_vecloop(6, .lenfn = base::nrow)) {
+		print(a); if ((i <- i - 1) > 0) {} else break
+	})
+})
+#|         name height  mass  hair_color skin_color eye_color birth_year    sex    gender homeworld species
+#|       <char>  <int> <num>      <char>     <char>    <char>      <num> <char>    <char>    <char>  <char>
+#| 1: Owen Lars    178   120 brown, grey      light      blue         52   male masculine  Tatooine   Human
+#|                  name height  mass hair_color skin_color eye_color birth_year    sex   gender homeworld species
+#|                <char>  <int> <num>     <char>     <char>    <char>      <num> <char>   <char>    <char>  <char>
+#| 1: Beru Whitesun Lars    165    75      brown      light      blue         47 female feminine  Tatooine   Human
+#|      name height  mass hair_color skin_color eye_color birth_year    sex    gender homeworld species
+#|    <char>  <int> <num>     <char>     <char>    <char>      <num> <char>    <char>    <char>  <char>
+#| 1:  R5-D4     97    32       <NA> white, red       red         NA   none masculine  Tatooine   Droid
+#|                 name height  mass hair_color skin_color eye_color birth_year    sex    gender homeworld species
+#|               <char>  <int> <num>     <char>     <char>    <char>      <num> <char>    <char>    <char>  <char>
+#| 1: Biggs Darklighter    183    84      black      light     brown         24   male masculine  Tatooine   Human
+#|              name height  mass    hair_color skin_color eye_color birth_year    sex    gender homeworld species
+#|            <char>  <int> <num>        <char>     <char>    <char>      <num> <char>    <char>    <char>  <char>
+#| 1: Obi-Wan Kenobi    182    77 auburn, white       fair blue-gray         57   male masculine   Stewjon   Human
+#|                name height  mass hair_color skin_color eye_color birth_year    sex    gender homeworld species
+#|              <char>  <int> <num>     <char>     <char>    <char>      <num> <char>    <char>    <char>  <char>
+#| 1: Anakin Skywalker    188    84      blond       fair      blue       41.9   male masculine  Tatooine   Human
+#|              name height  mass   hair_color skin_color eye_color birth_year    sex    gender homeworld species
+#|            <char>  <int> <num>       <char>     <char>    <char>      <num> <char>    <char>    <char>  <char>
+#| 1: Wilhuff Tarkin    180    NA auburn, grey       fair      blue         64   male masculine    Eriadu   Human
+#|         name height  mass hair_color skin_color eye_color birth_year    sex    gender homeworld species
+#|       <char>  <int> <num>     <char>     <char>    <char>      <num> <char>    <char>    <char>  <char>
+#| 1: Chewbacca    228   112      brown    unknown      blue        200   male masculine  Kashyyyk Wookiee
+#|        name height  mass hair_color skin_color eye_color birth_year    sex    gender homeworld species
+#|      <char>  <int> <num>     <char>     <char>    <char>      <num> <char>    <char>    <char>  <char>
+#| 1: Han Solo    180    80      brown       fair     brown         29   male masculine  Corellia   Human
+
+
+#: 把数据集当作向量循环播放（有断点） - 列
+base::list2env(base::list(i = 9)) |> base::with({
+	coro::loop(for (a in base::as.list(src) |> generate_vecloop(6)) {
+		str(a); if ((i <- i - 1) > 0) {} else break
+	})
+})
+#| List of 1
+#|  $ eye_color: chr [1:87] "blue" "yellow" "red" "yellow" ...
+#| List of 1
+#|  $ birth_year: num [1:87] 19 112 33 41.9 19 52 47 NA 24 57 ...
+#| List of 1
+#|  $ sex: chr [1:87] "male" "none" "none" "male" ...
+#| List of 1
+#|  $ gender: chr [1:87] "masculine" "masculine" "masculine" "masculine" ...
+#| List of 1
+#|  $ homeworld: chr [1:87] "Tatooine" "Tatooine" "Naboo" "Tatooine" ...
+#| List of 1
+#|  $ species: chr [1:87] "Human" "Droid" "Droid" "Human" ...
+#| List of 1
+#|  $ name: chr [1:87] "Luke Skywalker" "C-3PO" "R2-D2" "Darth Vader" ...
+#| List of 1
+#|  $ height: int [1:87] 172 167 96 202 150 178 165 97 183 182 ...
+#| List of 1
+#|  $ mass: num [1:87] 77 75 32 136 49 120 75 32 84 77 ...
+
