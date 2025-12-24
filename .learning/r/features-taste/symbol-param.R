15d274557140712a67c81fb287432b9da87431a2
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2025-12-24 16:23:53 +0800

    Update symbol-param.R

--- .learning/r/features-taste/symbol-param.R
+++ .learning/r/features-taste/symbol-param.R
@@ -3,11 +3,11 @@
 base::deparse(base::substitute(A)) #> "A"
 
 #: Using on param
-as.text.param = \ (symb) 
+as.text.param = \ (expr) 
 {
-	symb_astext = base::deparse(base::substitute(symb))
-	usethis::ui_info("The param as text is: {usethis::ui_value(symb_astext)}")
-	base::return(base::invisible(symb_astext))
+	expr_astext = base::deparse(base::substitute(expr))
+	usethis::ui_info("The param as text is: {usethis::ui_value(expr_astextx}")
+	base::return(base::invisible(expr_astextx)
 }
 
 #: Try
@@ -40,5 +40,24 @@ as.text.param(base::list(base::c(a,b),ccc,1)) |> print()
 
 #: Just expression codings in param
 #:  becoming it's text here !!
+
+
+#: rlang - symb & expr
+
+#: Using basic: inner function define
+param_exprs_catch = function (...) rlang::enexprs(...)
+param_symbs_catch = function (...) rlang::ensyms(...)
+#: Those will return a list of expression(s) / symbol(s)
+
+#: Trans more param to text
+param_exprs_astext = function (..., .map_fn = purrr::map_chr) rlang::enexprs(...) |> .map_fn(base::deparse)
+param_symbs_astext = function (..., .map_fn = purrr::map_chr) rlang::ensyms(...) |> .map_fn(base::deparse)
+
+#| > param_exprs_astext(a = a+-b, c*-d)
+#|        a          
+#| "a + -b" "c * -d" 
+#| > param_symbs_astext(x = a, y = b, c) #: `rlang::ensyms` can only catch symbol(s), expression(s) could met error.
+#|   x   y     
+#| "a" "b" "c" 
 
 
