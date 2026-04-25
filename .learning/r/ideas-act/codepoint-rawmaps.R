156145fa0115587e83ff9fd308e1d2f5d24703b7
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2026-04-25 17:47:24 +0800

    Update codepoint-rawmaps.R

--- .learning/r/ideas-act/codepoint-rawmaps.R
+++ .learning/r/ideas-act/codepoint-rawmaps.R
@@ -1,7 +1,54 @@
+#' @example base::c(0x6719L, 0x6c49L) |> magrittr::'%T>%'({ base::class(.) = 'codepoint' }) |> base::print()
+print.codepoint <- \ (codepoint) base::as.hexmode(codepoint) |> 
+	base::toupper() |> 
+	# 附各值首
+	purrr::map(~ .x |> base::append(after = 0, 'U+')) |> 
+	purrr::map_chr(~ .x |> base::paste(collapse = '')) |> 
+	# 附此目首
+	base::append(after = 0, 'codepoint') |> 
+	base::append(after = 1, glue::glue_safe(
+		n = base::length(codepoint), '[1:{n}]')) |> 
+	# 显
+	magrittr::'%T>%'(glue:::print.glue(sep = ' ')) |> 
+	# 果不見
+	base::invisible()
 
-#' @example base::c('0x6719', '0x6c49') |> codepoint.gen_utf8_text() #> "朙汉"
-codepoint.gen_utf8_text <- function (codepoints) codepoints |> 
-	# 毋使谬
+#' @example 
+#' base::list(a = base::c(0x6719L, 0x6c49L), b = 0x6719L) |> 
+#' 	magrittr::'%T>%'({ base::class(.) = 'codepoint.ls' }) |> 
+#' 	base::print()
+#' @example 
+#' base::list(a = base::c(0x6719L, 0x6c49L), b = 0x6719L) |> 
+#' 	purrr::map(~ .x |> magrittr::'%T>%'({ base::class(.) = 'codepoint' })) |> 
+#' 	magrittr::'%T>%'({ base::class(.) = 'codepoint.ls' }) |> 
+#' 	base::print()
+print.codepoint.ls <- print.codepoints <- \ (
+		x, 
+		.titleid_specify = x |> 
+			purrr::map_chr(base::class) |> 
+			tools::toTitleCase() |> 
+			base::unique() |> 
+			base::paste(collapse = '/') |> 
+			base::invisible(), 
+		.n = base::names(x)) x |> 
+	# 当致
+	purrr::map(~ .x |> magrittr::'%T>%'({ base::class(.) <- base::c('codepoint') })) |> 
+	# 成各自指
+	printool_outpre.list_namemark(.capby = base::print, .n = .n) |> 
+	# 结首在尾 示
+	magrittr::'%T>%'({ . |> 
+		printool_outpre.list_headmark(
+			.titleid_specify = .titleid_specify, 
+			.n = .n) |> 
+		base::cat()}) |> 
+	# 果当不見
+	base::invisible()
+
+#' @example base::c('0x6719', '0x6c49') |> codepointhex.as_codepoint() #> codepoint [1:2] U+6719 U+6C49
+#' @example base::c(0x6719, 0x6c49) |> base::as.integer() |> codepointhex.as_codepoint() #> codepoint [1:2] U+6719 U+6C49
+#' @example base::c(0x6719L, 0x6c49L) |> codepointhex.as_codepoint() #> codepoint [1:2] U+6719 U+6C49
+codepointhex.as_codepoint <- function (codepointhex) codepointhex |> 
+	# 毋使谬 转
 	magrittr::'%>%'(
 	{
 		if 
@@ -9,20 +56,39 @@ codepoint.gen_utf8_text <- function (codepoints) codepoints |>
 			(base::is.integer(.)) . else if 
 			(T) base::stop('utf8 code point must be int (hex chr support)')
 	}) |> 
-	# 呈若显
-	magrittr::'%T>%'(
-	{
-		base::as.hexmode(.) |> 
-			base::toupper() |> 
-			purrr::map(~ .x |> base::append(after = 0, 'U+')) |> 
-			purrr::map_chr(~ .x |> base::paste(collapse = '')) |> 
-			base::append(after = 0, 'Code Point(s) Input:') |> 
-			usethis::ui_info()
-	}) |> 
-	# 得字
-	base::intToUtf8() |> 
+	# 是目 及型
+	magrittr::'%T>%'({ base::class(.) <- base::c('codepoint') }) |> 
+	# 出
+	base::identity()
+
+#' @example base::c(a = 0x6719L, b = 0x6c49L) |> codepointhexls.as_codepoints() #> Codepoint List: a: U+6719; b: U+6C49
+#' @example base::list(a = base::c(0x6719L, 0x6c49L), b = 0x6719L) |> codepointhexls.as_codepoints() #> Codepoint List: a: U+6719, U+6C49; b: U+6719
+codepointhexls.as_codepoints <- function (codepointhex.ls) codepointhex.ls |> 
+	purrr::map(~ .x |> 
+		# 转 使正
+		codepointhex.as_codepoint() |> 
+		base::invisible()) |> 
+	# 是列 及型
+	magrittr::'%T>%'({ base::class(.) <- base::c('codepoint.ls', 'codepoints') }) |> 
 	# 是止
 	base::identity()
+
+
+
+#' @example base::c('0x6719', '0x6c49') |> codepoint.gen_utf8_text() #> "朙" "汉"
+#' @example base::c(0x6719, 0x6c49) |> base::as.integer() |> codepoint.gen_utf8_text() #> "朙" "汉"
+#' @example base::c(a = 0x6719L, b = 0x6c49L) |> codepoint.gen_utf8_text() #> a: "朙", b: "汉"
+#' @example base::list(a = base::c(0x6719L, 0x6c49L), b = 0x6719L) |> codepoint.gen_utf8_text() #> a: "朙汉", b: "朙"
+codepoint.gen_utf8_text <- function (codepoints, .show_input = T) codepoints |> 
+	# 使正
+	codepointhexls.as_codepoints() |> 
+	# 若呈
+	magrittr::'%T>%'({ if (base::isTRUE(.show_input)) base::print(.) }) |> 
+	# 得字
+	purrr::map_chr(base::intToUtf8) |> 
+	# 是止
+	base::identity()
+
 
 #' @describe make encoding-raw text as raw type in ls
 #' @example base::c('e6 9c 99','e6 b1 89') |> rawtexts.as_raws() #: Raw List of 2
@@ -38,16 +104,16 @@ rawtexts.as_raws <- function (
 	# 至
 	base::identity()
 
-#' @describe let raw-ls has its show
-#' @example base::c('e6 9c 99','e6 b1 89') |> rawtexts.as_raws() #: see the show
-#' @example base::c(a = 'e6 9c 99',b = 'e6 b1 89') |> rawtexts.as_raws() #: see the named show
-print.raw.ls = \ (x, .n = base::names(x)) xx |> 
+
+printool_outpre.list_namemark <- \ (
+		x, 
+		.capby = base::print, 
+		.n = base::names(x)) x |> 
 	# 暂去其名 使能元令
 	magrittr::'%T>%'({ base::names(.) <- NULL }) |> 
-	# 成身
-	purrr::map(~ utils::str(.x) |> utils::capture.output()) |> 
-	# 名身
-	purrr::map(~ base::list(raw = .x)) |> 
+	# 成身名身
+	purrr::map(~ .capby(.x) |> utils::capture.output()) |> 
+	purrr::map(~ base::list(content = .x |> base::trimws('both'))) |> 
 	# 加令元令
 	purrr::imap(\ (.x, .i) .x |> magrittr::'%T>%'({ .$index <- .i })) |> 
 	magrittr::'%T>%'({ base::names(.) <- .n }) |> 
@@ -65,23 +131,59 @@ print.raw.ls = \ (x, .n = base::names(x)) xx |>
 			base::identity()
 	})) |> 
 	# 合身与令
-	purrr::map(~ .x |> glue::glue_data_safe('{.i}:{raw}')) |> 
+	purrr::map(~ .x |> glue::glue_data_safe('{.i}: {content}')) |> 
 	purrr::map(~ '$' |> base::paste(.x)) |> 
-	# 结首在尾
-	magrittr::'%T>%'(
+	# 出
+	base::identity()
+
+printool_outpre.list_headmark <- \ (
+		x, 
+		.titleid_specify = x |> 
+			purrr::map_chr(base::class) |> 
+			tools::toTitleCase() |> 
+			base::unique() |> 
+			base::paste(collapse = '/') |> 
+			base::invisible(), 
+		.n = base::names(x)) x |> 
+	magrittr::'%>%'(
 	{
 		(if (!base::is.null(.n)) 'Named') |> 
-			base::c('Raw List of') |> 
+			base::c(.titleid_specify) |> 
+			base::c('List of') |> 
 			base::c(base::length(.)) |> 
-			# 合部为篱
+			# 合部为篱首
 			base::paste(collapse = ' ') |> 
 			glue::glue_safe(head = _, '>>> {head} <<<') |> 
-			# 合余为至
+			# 合余至
 			base::c(.) |> base::c('') |> 
 			base::paste(collapse = '\n') |> 
-			# 示
-			base::cat()
+			# 不示
+			base::invisible()
 	}) |> 
+	# 出
+	base::identity()
+
+
+#' @describe let raw-ls has its show
+#' @example base::c('e6 9c 99','e6 b1 89') |> rawtexts.as_raws() #: see the show
+#' @example base::c(a = 'e6 9c 99',b = 'e6 b1 89') |> rawtexts.as_raws() #: see the named show
+print.raw.ls <- print.raws <- \ (
+		x, 
+		.titleid_specify = x |> 
+			purrr::map_chr(base::class) |> 
+			tools::toTitleCase() |> 
+			base::unique() |> 
+			base::paste(collapse = '/') |> 
+			base::invisible(), 
+		.n = base::names(x)) x |> 
+	# 成各自指
+	printool_outpre.list_namemark(.capby = utils::str, .n = .n) |> 
+	# 结首在尾 示
+	magrittr::'%T>%'({ . |> 
+		printool_outpre.list_headmark(
+			.titleid_specify = .titleid_specify, 
+			.n = .n) |> 
+		base::cat()}) |> 
 	# 果当不見
 	base::invisible()
 
