b1906f13bcb13fdbdbfeaf20f585f4716b2c8fee
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2026-05-23 22:04:17 +0800

    Create git-mirror.sh

---
+++ .learning/shell/features-taste/git-mirror.sh
@@ -0,0 +1,64 @@
+
+
+
+#: demo with repo: gh:SukiSU-Ultra/SukiSU-Ultra.git
+
+: got mirror
+(
+	mkdir -p -- SukiSU.Ultra-src && 
+	cd -- SukiSU.Ultra-src && 
+	git clone -v --mirror -- https://github.com/SukiSU-Ultra/SukiSU-Ultra.git && 
+	cd -- SukiSU-Ultra.git && 
+	
+	echo -- main dev | while read -r -- branch ; 
+	do git worktree add ../tree/"$branch" "$branch" ; done && 
+	
+	echo -- v4.1.2 v3.1.9 | while read -r -- tag ; 
+	do git worktree add ../tags/"$tag" "$tag" ; done && 
+	
+	# {
+	# 	git worktree add ../tree/main main ; 
+	# 	git worktree add ../tree/dev dev ; 
+	# 	git worktree add ../tags/v4.1.2 v4.1.2 ; 
+	# 	git worktree add ../tags/v3.1.9 v3.1.9 ; 
+	# } && 
+	
+	: )
+
+: update trees
+(
+	cd -- SukiSU.Ultra-src/SukiSU-Ultra.git && 
+	
+	: git remote update && # will error, need add : for go on.
+	#2> fatal: refusing to fetch into branch 'refs/heads/dev' checked out at '../tree/dev'
+	
+	find -- ../tree -maxdepth 1 -mindepth 1 -type d | 
+		while read -r -- path ; 
+		do 
+			( 
+				cd -- "$path" && 
+				git checkout --detach && 
+				: ) && 
+			(1>&2 echo - :detached: "$path") && 
+			:; 
+		done && 
+	
+	git remote update && 
+	(
+		1>&2 echo - :remote updated: "$(
+			read -r -- pwd < <(echo "$PWD") && 
+			echo "$(dirname "$pwd" | xargs basename)/$(basename "$pwd")" && 
+			: )" && 
+		: ) && 
+	
+	find -- ../tree -maxdepth 1 -mindepth 1 -type d | 
+		while read -r -- path ; 
+		do 
+			( 
+				cd -- "$path" && 
+				git checkout "$(basename "$path")" && 
+				: ) && 
+			(1>&2 echo - :checkouted: "$path" as "$(basename "$path")") && 
+			:; 
+		done && 
+	: )
