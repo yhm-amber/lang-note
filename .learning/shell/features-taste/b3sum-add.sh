dcefd4d4c8cbeb4ff595d4b7656e459a63bd45b5
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2026-05-24 17:44:29 +0800

    Update b3sum-add.sh

--- .learning/shell/features-taste/b3sum-add.sh
+++ .learning/shell/features-taste/b3sum-add.sh
@@ -1,12 +1,15 @@
 
 b3sum *.tar.xz | while read -r -- hash name ; 
 do 
+	read -r -- hashed_name < <(
+		echo "$(dirname "$name")/$(basename -s .tar.xz -- "$name") - b3_${hash}.tar.xz" && 
+		: ) && 
 	( 
 		1>&2 printf "--- :from: $name" && 
-		1>&2 printf " ~ :to: $(dirname "$name")/$(basename -s .tar.xz -- "$name") - b3_${hash}.tar.xz" && 
+		1>&2 printf " ~ :to: $hashed_name" && 
 		1>&2 echo && 
 		: ) && 
 	#: del the `:` if you really want those mv.
-	: mv "$name" "$(dirname "$name")/$(basename -s .tar.xz -- "$name") - b3_${hash}.tar.xz" && 
+	: mv "$name" "$hashed_name" && 
 	:; 
 done
