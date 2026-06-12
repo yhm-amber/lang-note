3bf60527e8a972c7c98b0ad3baee52e0500b5816
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2026-06-12 16:01:52 +0800

    Update dict-packplay.py

--- .learning/python/features-taste/dict-packplay.py
+++ .learning/python/features-taste/dict-packplay.py
@@ -46,4 +46,12 @@ yd = ns_dic(dict(
 #: then access entrys in this ns/yard ...
 print((yd.a(), yd.b(), yd.fnc()(60, 2))) #> (1, 2, 123)
 
+#:	函数 `type` 可以用来创建类，从而支持基于字典 (dict) 来建立具有相应属性 (attribute) 的实例。此处有语法糖：
+#:	-	类的属性 (attribute) 其实为字典：
+#:		-	有 `module_name.x = 1` 即 `module_name.__dict__["x"] = 1` - https://docs.python.org/3/reference/datamodel.html?highlight=dictionary#modules
+#:		-	有 `ClassName.x` 即 `ClassName.__dict__["x"]` - https://docs.python.org/3/reference/datamodel.html?highlight=dictionary#custom-classes
+#:	-	而依此类所建实例 (instance) 的方法 (method) 亦实为类属性中的函数、其语法糖与 R 语言基本对象系统中的管道 (pipe) 类似：
+#:		-	有 `instance_name.f(a)` 即 `ClassName.f(instance_name, a)` - https://docs.python.org/3/reference/datamodel.html#instance-methods
+#:		-	如 `instance_name |> f(a)` 即为 `instance_name |> f.class_name(a)` 即 `f.class_name(instance_name, a)` 在于 R 中（此 `instance_name` 对象于其 `class` 属性中需有 `'class_name'` 这个标记）
+
 
