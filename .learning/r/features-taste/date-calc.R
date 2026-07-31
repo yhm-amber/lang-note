2992c9844f2cd1981bd6d87852949f9941393330
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2026-07-31 15:52:23 +0800

    add demo: IBNR Factor Table Time Fills function
    Added IBNR Factor Table Time Fills function to fill the IBNR Factor table until a specified beginning date, supporting monthly granularity.

--- .learning/r/features-taste/date-calc.R
+++ .learning/r/features-taste/date-calc.R
@@ -167,3 +167,182 @@ demo[, date |> date2yearqtr(fmt)]
 
 
 
+
+#: -------------------- e.g., Date Complete - demo with IBNR ----------------------
+
+#'	@title IBNR Factor Table Time Fills
+#'	@name ibnrfct_prefill
+#'	
+#'	@param ibnr_factors {data.table}, ibnr_factors from old format which has fields: `benefit_type`, `year`, `month`, `count_factor`, `amount_factor`
+#'	@param beginning_till {date}, The earlist date for you, using to fill the previous months.
+#'	
+#'	@returns {data.table}, Dataset which has filled the time.
+#'	
+#'	@description
+#'	Fill the IBNR Factor table till the beginning date you specified. Only supports granularity of month.
+#'	
+#'	@examples
+#'	
+#'	ibnr_factors = data.table::data.table(
+#'		benefit_type = c('A','A','B'), 
+#'		year = 2021, 
+#'		month = 3:5, 
+#'		count_factor = exp(-pi) + 1, 
+#'		amount_factor = exp(-pi) * 2 + 1)
+#'	
+#'	#: Just format back with nothing fills
+#'	ibnr_factors |> ibnrfct_prefill()
+#'	#: Fill to 2020-11-12
+#'	ibnr_factors |> ibnrfct_prefill('2020-11-12')
+#'	
+ibnrfct_prefill = function (
+		ibnr_factors, 
+		beginning_till = zoo::as.Date(NA)) 
+{
+	# error situation
+	{
+		# check ibnr_factors
+		{
+			needcol.ibnr_factors = c('benefit_type', 'year', 'month', 'count_factor', 'amount_factor')
+			colnames(ibnr_factors) = colnames(ibnr_factors) |> tolower()
+			if (all(needcol.ibnr_factors %in% colnames(ibnr_factors))) {
+				usethis::ui_done("Fields {usethis::ui_field(needcol.ibnr_factors)} found in IBNR factors input.")
+			} else {
+				.m = glue::glue(
+					"\\
+					Missing Column - IBNR factors input MUST has fields {
+					usethis::ui_field(needcol.ibnr_factors)} in it but only found [{
+					usethis::ui_field(colnames(ibnr_factors))}].")
+				usethis::ui_oops(.m)
+				stop(.m)
+			}
+		}
+		
+		# check the beginning_till
+		{
+			allowclass.beginning_till = c('Date', 'character')
+			if (any(class(beginning_till) %in% allowclass.beginning_till)) {
+				usethis::ui_done("Class of inputed begin-date {usethis::ui_value(class(beginning_till))} is supported (which is in [{usethis::ui_value(allowclass.beginning_till)}]).")
+			} else {
+				.m = glue::glue(
+					"\\
+					Class of inputed begin-date {
+					usethis::ui_value(class(beginning_till))} is NOT supported (which is not in [{
+					usethis::ui_value(allowclass.beginning_till)}]).")
+				usethis::ui_oops(.m)
+				stop(.m)
+			}
+		}
+	}
+	
+	# ready fn
+	{
+		add_mo = \ (date, mo) "lubridate" |> withr::with_package(date %m+% lubridate::period(month = if (length(mo) > 0) mo else as.integer(NA)))
+		add_dy = \ (date, dy) "lubridate" |> withr::with_package(date %m+% lubridate::period(day = if (length(dy) > 0) dy else as.integer(NA)))
+		add_yr = \ (date, year) date |> add_mo(year * 12)
+		
+		add_mo_posi = \ (date, mo) date |> add_mo(mo |> purrr::keep(~ .x >= 0))
+		add_dy_posi = \ (date, dy) date |> add_dy(dy |> purrr::keep(~ .x >= 0))
+		add_yr_posi = \ (date, year) date |> add_yr(year |> purrr::keep(~ .x >= 0))
+	}
+	
+	# param type
+	{
+		data.table::setDT(ibnr_factors)
+		beginning_till = zoo::as.Date(beginning_till)
+	}
+	
+	# init dataset
+	{
+		ibnr_factors[, origin_date := zoo::as.Date(zoo::as.yearmon(.SD |> glue::glue_data('{year}-{month}')))]
+		ibnr_factors[, time_corresponding := origin_date |> format("%Y/%m")]
+		ibnr_factors[, bnf_id := benefit_type]
+	}
+	
+	# make earlist of input dataset
+	{
+		benefit_earlist = ibnr_factors[, .(origin_earlist = min(na.rm = T, origin_date)), by = bnf_id]
+		benefit_earlist[, beginning_till := pmin(na.rm = T, origin_earlist, beginning_till)]
+	}
+	
+	
+	# calc diff auto
+	{
+		benefit_earlist[, earlier_mon.diff := (zoo::as.yearmon(origin_earlist) - zoo::as.yearmon(beginning_till)) * 12]
+		benefit_earlist[, earlier_mon := beginning_till |> purrr::map2(earlier_mon.diff, ~ .x |> add_mo_posi(pmin(na.rm = T, 0, .y - 1):(.y - 1)) |> format("%Y%m"))]
+	}
+	
+	# unwarp res list
+	{
+		benefit_earlier = benefit_earlist[, purrr::map2(
+			bnf_id, 
+			earlier_mon, 
+			~ tidyr::crossing(bnf_id = .x, time_corresponding = .y)) |> 
+				data.table::rbindlist()][!is.na(time_corresponding)]
+		
+		benefit_earlier[, let(count_factor = 1, amount_factor = 1)]
+	}
+	
+	# rbind input
+	{
+		ibnr_factors_full = data.table::setDT(ibnr_factors)[, .(bnf_id, time_corresponding, count_factor, amount_factor)] |> 
+			list(benefit_earlier) |> 
+			data.table::rbindlist() |> 
+			data.table::setorder(bnf_id, time_corresponding)
+	}
+	
+	# select fields and return
+	{
+		ibnr_factors_full[, .(
+			benefit_type = bnf_id, 
+			time_corresponding, 
+			ibnr_factor.a = amount_factor, 
+			ibnr_factor.c = count_factor)]
+	}
+}
+
+
+
+
+ibnr_factors = data.table::data.table(
+	benefit_type = c('A','A','B'), 
+	year = 2021, 
+	month = 3:5, 
+	count_factor = exp(-pi) + 1, 
+	amount_factor = exp(-pi) * 2 + 1) |> print()
+#|	   benefit_type  year month count_factor amount_factor
+#|	         <char> <num> <int>        <num>         <num>
+#|	1:            A  2021     3     1.043214      1.086428
+#|	2:            A  2021     4     1.043214      1.086428
+#|	3:            B  2021     5     1.043214      1.086428
+
+#: Just format back with nothing fills
+ibnr_factors |> ibnrfct_prefill()
+#|	✔ Fields benefit_type, year, month, count_factor, amount_factor found in IBNR factors input.
+#|	✔ Class of inputed begin-date 'Date' is supported (which is in ['Date', 'character']).
+#|	   benefit_type time_corresponding ibnr_factor.a ibnr_factor.c
+#|	         <char>             <char>         <num>         <num>
+#|	1:            A            2021/03      1.086428      1.043214
+#|	2:            A            2021/04      1.086428      1.043214
+#|	3:            B            2021/05      1.086428      1.043214
+
+#: Fill to 2020-11-12
+ibnr_factors |> ibnrfct_prefill('2020-11-12')
+#|	✔ Fields benefit_type, year, month, count_factor, amount_factor found in IBNR factors input.
+#|	✔ Class of inputed begin-date 'character' is supported (which is in ['Date', 'character']).
+#|	    benefit_type time_corresponding ibnr_factor.a ibnr_factor.c
+#|	          <char>             <char>         <num>         <num>
+#|	 1:            A             202011      1.000000      1.000000
+#|	 2:            A             202012      1.000000      1.000000
+#|	 3:            A            2021/03      1.086428      1.043214
+#|	 4:            A            2021/04      1.086428      1.043214
+#|	 5:            A             202101      1.000000      1.000000
+#|	 6:            A             202102      1.000000      1.000000
+#|	 7:            B             202011      1.000000      1.000000
+#|	 8:            B             202012      1.000000      1.000000
+#|	 9:            B            2021/05      1.086428      1.043214
+#|	10:            B             202101      1.000000      1.000000
+#|	11:            B             202102      1.000000      1.000000
+#|	12:            B             202103      1.000000      1.000000
+#|	13:            B             202104      1.000000      1.000000
+
